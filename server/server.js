9b808e1d67e64c64305bacfe8c65a5daac10ab39
tonyOP1985 <toliverpaull@gmail.com>
2017-08-21 21:01:58 -0400

    Add GET, GET by id, DELETE by id, PATCH by id routes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// const _ = require('lodash');
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -13,6 +13,7 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+
 //create new musicians
 app.post('/musicians', (req, res) => {
   console.log(req.body);
@@ -26,6 +27,82 @@ app.post('/musicians', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.send(400).send(e);
+  });
+});
+
+
+//returns all musicians
+app.get('/musicians', (req, res) => {
+  Musician.find().then((musicians) => {
+    res.send({musicians});
+  }, (e) => {
+    res.status(400).send(e);
+  }).catch((e) => {
+    console.log(e);
+  });
+});
+
+
+//return musician by id
+app.get('/musicians/:id', (req, res) => {
+  var id = req.params.id;
+  //invalid id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  //incorrect valid id
+  Musician.findById(id).then((musician) => {
+    if(!musician) {
+      return res.status(404).send();
+    }
+    //correct id
+    res.send({musician});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
+//delete musicians
+app.delete('/musicians/:id', (req, res) => {
+ var id = req.params.id;
+ //invalid id
+ if (!ObjectID.isValid(id)) {
+   return res.status(404).send();
+ }
+ //incorrect valid id
+ Musician.findByIdAndRemove(id).then((musician) => {
+   if (!musician) {
+     return res.status(404).send();
+   }
+   //correct id
+   res.send({musician});
+ }).catch((e) => {
+   res.status(400).send();
+ });
+});
+
+
+//update musician
+app.patch('/musicians/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['musician', 'instruments']);
+
+  //invalid id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  //need to implement update for dates
+
+  Musician.findByIdAndUpdate(id, {$set: body}, {new: true}).then((musician) => {
+    if (!musician) {
+      return res.status(404).send();
+    }
+
+    res.send({musician});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
